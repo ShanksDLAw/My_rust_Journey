00850b90d6a1ebb4bf414d5a5dfa84d2b5eb682d
ShanksDLAw <aabduljalaal@yahoo.com>
2023-04-21 17:53:39 +0100

    Add files via upload

---
+++ main.rs
@@ -0,0 +1,28 @@
+fn main() {
+
+    //Basic Variable Declaration
+    // All variables are immutable by default and to make it mutable use
+    //Use the key word 'mut'
+    // to declare a variable use the keyword 'let'
+    let y = 60;
+    let x = 45;
+    let sum = y + x;
+
+    let mut z = 12;
+    z = 15;
+
+
+    println!("The sum of these numbers are {}", sum);
+    println!("The Value of z is {}", z);
+
+    // Data Types
+    //In Rust the data types are denoted (numerical data types) as either 
+    //either an unsigned integer (u) or signed integter
+    //examples u32 - unsigned integer of size 32 bits
+    // examples i32 - signed integer of size 32 bit
+    // For Float can be f32 or f64
+
+
+
+
+}
